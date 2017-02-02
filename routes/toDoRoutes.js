9ae8e954d9664c0e136ea6115bffb397fd5fca6a
terrell linnell <terrell.linnell@gmail.com>
2017-02-02 11:47:14 -0700

    got the route '/' working; missed some imports and exports

--- routes/toDoRoutes.js
+++ routes/toDoRoutes.js
@@ -4,30 +4,37 @@ var express = require('express');
 var router = new express.Router();
 
 router.route('/')
+  .get(function(req, res){
+    res.json({message: 'Its working'})
+  })
 
 
-router.route('/api')
-app.get('/api/toDos',function(req, res){
-  toDos.find(function(err, data){
-    if(err){
-      console.log("error finding todos");
-    }
-    else{
-      re.render('toDos', {toDos: data});
-    }
-  })
-})
-app.post('/api/toDos', function(req, res){
-  var toDo = new toDo({
-    name:req.body.name,
-    date:req.body.date,
-  })
-  toDo.save(function(err, toDoData){
-    if (err) {
-      console.log("Error with toDo");
-    }
-    else{
-      res.json(toDoData);
-    }
-  })
-});
+// router.route('/api')
+// app.get('/api/toDos',function(req, res){
+//   toDos.find(function(err, data){
+//     if(err){
+//       console.log("error finding todos");
+//     }
+//     else{
+//       re.render('toDos', {toDos: data});
+//     }
+//   })
+// })
+// app.post('/api/toDos', function(req, res){
+//   var toDo = new toDo({
+//     name:req.body.name,
+//     date:req.body.date,
+//   })
+//   toDo.save(function(err, toDoData){
+//     if (err) {
+//       console.log("Error with toDo");
+//     }
+//     else{
+//       res.json(toDoData);
+//     }
+//   })
+// });
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var express = require('express');
 var app = express();
 var toDos = require('./models/toDos');
 var mongoose = require('mongoose');
+var toDoRoutes = require('./routes/toDoRoutes');
+var bodyParser = require('body-parser');
 
+mongoose.connect("mongodb://localhost/Todos");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-
+app.use('/api', toDoRoutes);
 
 
 
