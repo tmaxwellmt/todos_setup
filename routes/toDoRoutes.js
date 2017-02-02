1d82b4504fb5c7456837ba1ad98079cce26fbf0f
tmaxwellmt <tmaxwellmt@gmail.com>
2017-02-02 15:06:36 -0700

    created api endpoints

--- routes/toDoRoutes.js
+++ routes/toDoRoutes.js
@@ -12,47 +12,58 @@ router.route('/')
       }
     })
   })
-
   .post(function (req, res){
     var toDo = new ToDo({
       name: req.body.name,
       date: req.body.date,
   })
+
   toDo.save(function (err, toDoData) {
     if (err) {
-    console.log("error finding todos");
-    } else{
-      res.json(toDoData);
-    }
+      console.log("error finding todos");
+      } else{
+        res.json(toDoData);
+      }
+    })
+  });
+
+  router.route('/:todo_id')
+    .get(function(req, res){
+      ToDo.findById(req.params.todo_id, function (err, toDoData) {
+        if (err) {
+          console.log("error finding todo");
+        } else {
+          res.json(toDoData);
+        }
+      })
+    })
+
+  .put(function (req, res) {
+    ToDo.findById(req.params.todo_id, function(err, toDoData){
+        if (err) {
+          console.log("error finding a specific todo");
+        } else {
+        toDoData.name = req.body.name ? req.body.name : toDoData.name;
+        toDoData.date = req.body.date ? req.body.date : toDoData.date;
+
+        toDoData.save(function (e, updatedToDo) {
+          if (e) {
+              console.log("error with updated todo");
+          } else {
+              res.json(updatedToDo);
+          }
+        });
+      }
+    });
   })
-})
-
-// router.route('/api')
-// app.get('/api/toDos',function(req, res){
-//   toDos.find(function(err, data){
-//     if(err){
-//       console.log("error finding todos");
-//     }
-//     else{
-//       re.render('toDos', {toDos: data});
-//     }
-//   })
-// })
-// app.post('/api/toDos', function(req, res){
-//   var toDo = new toDo({
-//     name:req.body.name,
-//     date:req.body.date,
-//   })
-//   toDo.save(function(err, toDoData){
-//     if (err) {
-//       console.log("Error with toDo");
-//     }
-//     else{
-//       res.json(toDoData);
-//     }
-//   })
-// });
-
-
+  .delete(function (req, res) {
+    ToDo.remove({_id: req.params.todo_id}, function(err, t) {
+      if (err) {
+        console.log(err, "could not delete todo!!!!");
+      } else {
+        res.json({message: "ToDo deleted"})
+      }
+    });
+  });
 
 module.exports = router;
