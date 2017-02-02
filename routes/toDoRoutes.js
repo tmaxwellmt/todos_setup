26362a2490d0ce31176229ec133bae2001d45706
tmaxwellmt <tmaxwellmt@gmail.com>
2017-02-02 15:32:08 -0700

    finished todo app with comments

--- routes/toDoRoutes.js
+++ routes/toDoRoutes.js
@@ -1,24 +1,25 @@
-var express = require('express');
-var router = new express.Router();
-var ToDo = require('../models/toDos');
+var express = require('express');// imports express package
+var router = new express.Router();//creates a new router
+var ToDo = require('../models/toDos');//gives access to toDos.js files in models directory
 
+//creates new route at /api "/"
 router.route('/')
   .get(function(req, res){
-    ToDo.find(function(err, data) {
+    ToDo.find(function(err, data) { // finds all todos
       if (err) {
-      console.log("error finding todos");
+      console.log("error finding todos");//logs erros to console
       } else{
-        res.json(data);
+        res.json(data); //returns json data
       }
     })
   })
-  .post(function (req, res){
-    var toDo = new ToDo({
+  .post(function (req, res){ // creates new todo
+    var toDo = new ToDo({ // states how we are making todos
       name: req.body.name,
       date: req.body.date,
   })
 
-  toDo.save(function (err, toDoData) {
+  toDo.save(function (err, toDoData) {//saves todo to database
     if (err) {
       console.log("error finding todos");
       } else{
@@ -26,10 +27,10 @@ router.route('/')
       }
     })
   });
-
+//creates a new route at /api/toDos.todo_id
   router.route('/:todo_id')
-    .get(function(req, res){
-      ToDo.findById(req.params.todo_id, function (err, toDoData) {
+    .get(function(req, res){// finds all current todos
+      ToDo.findById(req.params.todo_id, function (err, toDoData) { //finds todo's id
         if (err) {
           console.log("error finding todo");
         } else {
@@ -38,15 +39,15 @@ router.route('/')
       })
     })
 
-  .put(function (req, res) {
-    ToDo.findById(req.params.todo_id, function(err, toDoData){
+  .put(function (req, res) { // updates an existing todo
+    ToDo.findById(req.params.todo_id, function(err, toDoData){ // finds a todo's id
         if (err) {
           console.log("error finding a specific todo");
         } else {
-        toDoData.name = req.body.name ? req.body.name : toDoData.name;
-        toDoData.date = req.body.date ? req.body.date : toDoData.date;
+        toDoData.name = req.body.name ? req.body.name : toDoData.name; //type new name if new name is old name  keep old name if not update it to the new name
+        toDoData.date = req.body.date ? req.body.date : toDoData.date;// same above with date, like if else statement
 
-        toDoData.save(function (e, updatedToDo) {
+        toDoData.save(function (e, updatedToDo) { //saves new updated todo to database
           if (e) {
               console.log("error with updated todo");
           } else {
@@ -56,8 +57,8 @@ router.route('/')
       }
     });
   })
-  .delete(function (req, res) {
-    ToDo.remove({_id: req.params.todo_id}, function(err, t) {
+  .delete(function (req, res) { // deletes a specific todo
+    ToDo.remove({_id: req.params.todo_id}, function(err, t) { // have to have id as object because thats what json expects this data to be
       if (err) {
         console.log(err, "could not delete todo!!!!");
       } else {
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express = require('express');//requires express package
+var express = require('express');//imports express package
 var app = express();//starts express server
-var toDos = require('./models/toDos');//requires file models.todo
-var mongoose = require('mongoose'); // requires models for mongoose
-var toDoRoutes = require('./routes/toDoRoutes'); // require Api route
-var bodyParser = require('body-parser');
+var toDos = require('./models/toDos');//imports file models.todo
+var mongoose = require('mongoose'); // imports mongoose package
+var toDoRoutes = require('./routes/toDoRoutes'); // imports routes directory and file
+var bodyParser = require('body-parser'); //imports body parser package
 
-mongoose.connect("mongodb://localhost/Todos");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost/Todos"); //connects mongoose to mongodb
+app.use(bodyParser.json());//needed to read req.body
+app.use(bodyParser.urlencoded({extended: true}));//needed
 
-app.use('/api/toDos', toDoRoutes);
-
-
+app.use('/api/toDos', toDoRoutes);//sets up the route so '/api/toDos' is always added
 
 
 
-app.listen(3000, function () {
+
+
+app.listen(3000, function () {//says webpage will run on port3000 and do function on start
   console.log('Express server is up and running on port 3000')
 });
