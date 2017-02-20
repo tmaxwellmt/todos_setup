0d1b492edb7ca5aeddd870ef113f77f4c5f4ebec
tmaxwellmt <tmaxwellmt@gmail.com>
2017-02-20 15:46:32 -0700

    its working on localhost

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');//imports express package
 var app = express();//starts express server
-var toDos = require('./models/toDos');//imports file models.todo
+var ToDo = require('./models/toDos');//imports file models.todo
 var mongoose = require('mongoose'); // imports mongoose package
 var toDoRoutes = require('./routes/toDoRoutes'); // imports all API routes
 var bodyParser = require('body-parser'); //imports body parser package
@@ -15,8 +15,14 @@ app.use(bodyParser.urlencoded({extended: true}));//needed
 app.use('/api/toDos', toDoRoutes);//sets up the route so '/api/toDos' is always added
 
 
-app.get("/", function(req, res) {
-  res.render('index', { welcome: "Hello, Please to Meet you" });
+app.get("/example", function(req, res) {
+  ToDo.find(function (err, toDos) {
+    if (err){
+      console.log(err);
+    }else {
+      res.render('index', {welcome: "Hello World", todos: toDos })
+    }
+  })
 });
 
 
